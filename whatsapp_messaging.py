a3544054061c32666ef6bbb38f9da4308e493ff7
Joni Lee <62823188+ChillySandwich@users.noreply.github.com>
2021-03-12 10:55:54 +1300

    Update whatsapp_messaging.py

--- whatsapp_messaging.py
+++ whatsapp_messaging.py
@@ -1,5 +1,8 @@
 from twilio.rest import Client
 
+
+#add in Twilio_sid and auth_token from Twilio account 
+
 def msg_friends(event=None, context=None):
 
     # get your sid and auth token from twilio
@@ -8,11 +11,10 @@ def msg_friends(event=None, context=None):
 
     whatsapp_client = Client(twilio_sid, auth_token)
 
-    # keep adding contacts to this dict to send
-    # them the message
+    # Add in names and numbers in list. Multiple names and numbers can be added.
     contact_directory = {'Name Goes Here' : 'Number goes here',}
 
-
+    # for each key - contact in the contact_directory, send the following message.
     for key, value in contact_directory.items():
         msg_the_friends = whatsapp_client.messages.create(
                 body = 'Beep beep {} ! Skeptics Subscription WhatsApp Group welcomes you once again. '.format(key),
